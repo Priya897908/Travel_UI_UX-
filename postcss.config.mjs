172cf6ea4ada3e88658562e875e4a3d84fc0e549
Priya897908 <129609999+Priya897908@users.noreply.github.com>
2025-02-06 19:59:13 +0530

    Add files via upload

---
+++ postcss.config.mjs
@@ -0,0 +1,12 @@
+// postcss.config.mjs
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+const config = {
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+  ],
+};
+
+export default config;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './app/**/*.{html,js,jsx,ts,tsx,vue}', // add the correct paths here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
